67c220e30824b45996c089f18eb2c99138a4b3af
Lindy <d4hkie@gmail.com>
2019-10-05 07:57:09 +0100

    Updates

---
+++ precache-manifest.ddebd9abcb4e85a2461d9f36b13ea179.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263a43d2a5183f0fa514e96dbbad60c1",
+    "url": "/react-pokeapp-test/index.html"
+  },
+  {
+    "revision": "23fd2504e05b179d4e7b",
+    "url": "/react-pokeapp-test/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "95f551c99d5f58ab118e",
+    "url": "/react-pokeapp-test/static/css/main.61086156.chunk.css"
+  },
+  {
+    "revision": "23fd2504e05b179d4e7b",
+    "url": "/react-pokeapp-test/static/js/2.b2ab8d3a.chunk.js"
+  },
+  {
+    "revision": "95f551c99d5f58ab118e",
+    "url": "/react-pokeapp-test/static/js/main.aa91688a.chunk.js"
+  },
+  {
+    "revision": "7543382fdb654ae4b22c",
+    "url": "/react-pokeapp-test/static/js/runtime-main.ef5b4310.js"
+  },
+  {
+    "revision": "831e1f4da157d96bc996f8c9f5f1e578",
+    "url": "/react-pokeapp-test/static/media/PokemonGB.831e1f4d.ttf"
+  },
+  {
+    "revision": "bc55a789b7841ed53f02bb57a5dcc4b6",
+    "url": "/react-pokeapp-test/static/media/bg.bc55a789.jpg"
+  }
+]);
